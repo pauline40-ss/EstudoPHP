a579c9a16adc63c067816425801f2e7648eb286d
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2022-11-17 10:06:39 -0300

    adicionado array filé Sabará

--- restaurante-menu.php
+++ restaurante-menu.php
@@ -8,15 +8,32 @@
     <title>Document</title>
 </head>
 
-<body>   
-    
-<?php  
+<body>
 
-$pratos= array( "file-sabara", "tilapia-tabuleiro", "costelinha-tiradentes", "serra-caraça", "frango-diamantina", "pastel-angu", "pernil-serro", "sobremesa - inconfidentes", "torta-ouropreto");
-  
-print_r($pratos);
+    <?php
 
-?>
+    $pratos = [
+        "file-sabara" => [
+            "titulo" => "PRATO FILÉ AO MOLHO SABARÁ",
+            "ingredientes" => " Medalhão de Filé, flambado na cachaça, com purê de mandioca ao molho de jabuticaba ",
+            "tempo" => "Tempo médio de preparo 30 minutos ",
+            "sugestao" => "Prato harmoniza com vinho branco seco",
+        ],
+
+        "tilapia-tabuleiro",
+        "costelinha-tiradentes",
+        "serra-caraca",
+        "frango-diamantina",
+        "pastel-angu",
+        "pernil-serro",
+        "sobremesa - inconfidentes",
+        "torta-ouropreto",
+    ];
+    echo "<pre>";
+    print_r($pratos);
+    echo "</pre>";
+
+    ?>
     <section id="file-sabara">
         <h2> <b> PRATO FILÉ AO MOLHO SABARÁ</b> </h2>
         <p><i> Medalhão de Filé, flambado na cachaça, com puré de mandioca ao molho de jabuticaba</i> </p>
@@ -28,67 +45,67 @@ print_r($pratos);
         <p><i> Filé de Tilápia no limão, acompanha salada de repolho com abacaxi e maçã e arroz branco ao alho</i> </p>
         <p> Tempo médio de preparo 20 minutos</p>
         <p> Prato harmoniza com vinho branco seco</p>
-
-        <section id=" costelinha-tiradentes">
+    </section>
+    <section id=" costelinha-tiradentes">
         <h2> <b> PRATO COSTELINHA A MODA TIRADENTES</b> </h2>
         <p><i> Costelinha banhada na cachaça, acompanha couve frita, lascas de mandioca com molho de pimenta biquinho</i> </p>
         <p> Tempo médio de preparo 40 minutos</p>
-        <p> Prato harmoniza com vinho suave</p>   
-        
-        <section id= " serra- caraça">
-        <h2> <b>PETISCO SERRA DO CARAÇA</b> </h2>
-        <p><i> Bolinho de arroz recheado com queijo e goiabada</i> </p>
-        <p> Tempo médio de preparo 20 minutos</p>
-        <p> Prato harmoniza com vinho de jabuticada</p>
-
-        <section id=" frango-diamantina">
-        <h2> <b>PRATO SURPRESA DIAMANTINA DE FRANGO</b> </h2>
-        <p><i> Bombom de frango, envolto em bacon e queijo banhado ao molho de mostarda e mel</i> </p>
-        <p> Tempo médio de preparo 20 minutos</p>
         <p> Prato harmoniza com vinho suave</p>
 
-        <section id=" pastel-angu">
-        <h2> <b> PETISCO PASTEL DE ANGU TROPEIRO</b> </h2>
-        <p><i> Sabores banana, carne, queijo, frango com passas , milho e queijo</i> </p>
-        <p> Tempo médio de preparo 15 minutos</p>
-        <p> Prato harmoniza com caldo de cana com limão ou abacaxi</p>
+        <section id=" serra-caraca">
+            <h2> <b>PETISCO SERRA DO CARAÇA</b> </h2>
+            <p><i> Bolinho de arroz recheado com queijo e goiabada</i> </p>
+            <p> Tempo médio de preparo 20 minutos</p>
+            <p> Prato harmoniza com vinho de jabuticada</p>
 
-        <section id="  pernil-serro">
-        <h2> <b> PRATO PERNIL AO MOLHO DE GOIABA DO SERRO </b> </h2>
-        <p><i> Pernil marinado por 24 horas em especiarias da casa, risoto de queijo do serro , com molho de goiaba</i> </p>
-        <p> Tempo médio de preparo 45 minutos</p>
-        <p> Prato harmoniza com vinho branco seco</p>
+            <section id=" frango-diamantina">
+                <h2> <b>PRATO SURPRESA DIAMANTINA DE FRANGO</b> </h2>
+                <p><i> Bombom de frango, envolto em bacon e queijo banhado ao molho de mostarda e mel</i> </p>
+                <p> Tempo médio de preparo 20 minutos</p>
+                <p> Prato harmoniza com vinho suave</p>
 
-        <section id="sobremesa-inconfidentes">
-        <h2> <b>SOBREMESA INCONFIDENTES </b> </h2>
-        <p><i> Sorvete de queijo artesanal, com geleia de goiaba e hortelã</i> </p>
-        <p> Tempo médio de preparo 20 minutos</p>
+                <section id=" pastel-angu">
+                    <h2> <b> PETISCO PASTEL DE ANGU TROPEIRO</b> </h2>
+                    <p><i> Sabores banana, carne, queijo, frango com passas , milho e queijo</i> </p>
+                    <p> Tempo médio de preparo 15 minutos</p>
+                    <p> Prato harmoniza com caldo de cana com limão ou abacaxi</p>
 
-        <section id=" torta-ouropreto">
-        <h2> <b>TORTA OURO PRETO</b> </h2>
-        <p><i> Torta de chocolate amargo, com frutas vermelhas</i> </p>
-        <p> Tempo médio de preparo 20 minutos</p>
-    
+                    <section id="  pernil-serro">
+                        <h2> <b> PRATO PERNIL AO MOLHO DE GOIABA DO SERRO </b> </h2>
+                        <p><i> Pernil marinado por 24 horas em especiarias da casa, risoto de queijo do serro , com molho de goiaba</i> </p>
+                        <p> Tempo médio de preparo 45 minutos</p>
+                        <p> Prato harmoniza com vinho branco seco</p>
+
+                        <section id="sobremesa-inconfidentes">
+                            <h2> <b>SOBREMESA INCONFIDENTES </b> </h2>
+                            <p><i> Sorvete de queijo artesanal, com geleia de goiaba e hortelã</i> </p>
+                            <p> Tempo médio de preparo 20 minutos</p>
+
+                            <section id=" torta-ouropreto">
+                                <h2> <b>TORTA OURO PRETO</b> </h2>
+                                <p><i> Torta de chocolate amargo, com frutas vermelhas</i> </p>
+                                <p> Tempo médio de preparo 20 minutos</p>
+
+                            </section>
+
+                        </section>
+
+
+
+
+                    </section>
+
+
+                </section>
+
+
+            </section>
+
         </section>
 
-        </section>
-
-        
 
 
-        </section>
-
-
-        </section>
-
-
-        </section>
-
-        </section>
-
-
-
-        </section>
+    </section>
 
 
     </section>
