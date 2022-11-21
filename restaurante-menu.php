64117245a5e1daac641e440701c6527bff377409
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2022-11-20 22:57:01 -0300

    exibição de pratos do array

--- restaurante-menu.php
+++ restaurante-menu.php
@@ -20,142 +20,84 @@
             "sugestao" => "Prato harmoniza com vinho branco seco",
         ],
 
-        "tilapia-tabuleiro"=> [
+        "tilapia-tabuleiro" => [
             "titulo" => "PRATO TILÁPIA A MODA CACHOEIRA TABULEIRO",
             "ingredientes" => " Filé de Tilápia no limão, acompanha salada de repolho com abacaxi e maçã e arroz branco ao alho ",
             "tempo" => "Tempo médio de preparo 20 minutos ",
             "sugestao" => "Prato harmoniza com vinho branco seco",
         ],
 
-        "costelinha-tiradentes"=>[
-            "titulo"=>"PRATO COSTELINHA A MODA TIRADENTES ",
-            "ingredientes"=> " Costelinha banhada na cachaça, acompanha couve frita, lascas de mandioca com molho de pimenta biquinho ",
-            "tempo"=>"Tempo médio de preparo 40 minutos",
-            "sugestao"=>"Prato harmoniza com vinho suave ",
+        "costelinha-tiradentes" => [
+            "titulo" => "PRATO COSTELINHA A MODA TIRADENTES ",
+            "ingredientes" => " Costelinha banhada na cachaça, acompanha couve frita, lascas de mandioca com molho de pimenta biquinho ",
+            "tempo" => "Tempo médio de preparo 40 minutos",
+            "sugestao" => "Prato harmoniza com vinho suave ",
         ],
 
-        "serra-caraca"=>[
-            "titulo"=>"PETISCO SERRA DO CARAÇA ",
-            "ingredientes"=>"Bolinho de arroz recheado com queijo e goiabada ",
-            "tempo"=>"Tempo médio de preparo 20 minutos",
-            "sugestao"=>"Prato harmoniza com vinho de jabuticada",
+        "serra-caraca" => [
+            "titulo" => "PETISCO SERRA DO CARAÇA ",
+            "ingredientes" => "Bolinho de arroz recheado com queijo e goiabada ",
+            "tempo" => "Tempo médio de preparo 20 minutos",
+            "sugestao" => "Prato harmoniza com vinho de jabuticada",
         ],
-        "frango-diamantina"=>[
-            "titulo"=>"PRATO SURPRESA DIAMANTINA DE FRANGO ",
-            "ingredientes"=>"Bombom de frango, envolto em bacon e queijo banhado ao molho de mostarda e mel ",
-            "tempo"=>"Tempo médio de preparo 20 minutos",
-            "sugestao"=>" Prato harmoniza com vinho suave",
-        ],
-
-        "pastel-angu"=>[
-            "titulo"=>"PETISCO PASTEL DE ANGU TROPEIRO ",
-            "ingredientes"=>" Sabores banana, carne, queijo, frango com passas , milho e queijo",
-            "tempo"=>" Tempo médio de preparo 15 minutos",
-            "sugestao"=>" Prato harmoniza com caldo de cana com limão ou abacaxi",
-        ],
-        "pernil-serro"=>[
-            "titulo"=>"PRATO PERNIL AO MOLHO DE GOIABA DO SERRO ",
-            "ingredientes"=>"Pernil marinado por 24 horas em especiarias da casa, risoto de queijo do serro , com molho de goiaba ",
-            "tempo"=>" Tempo médio de preparo 45 minutos",
-            "sugestao"=>"Prato harmoniza com vinho branco seco ",
+        "frango-diamantina" => [
+            "titulo" => "PRATO SURPRESA DIAMANTINA DE FRANGO ",
+            "ingredientes" => "Bombom de frango, envolto em bacon e queijo banhado ao molho de mostarda e mel ",
+            "tempo" => "Tempo médio de preparo 20 minutos",
+            "sugestao" => " Prato harmoniza com vinho suave",
         ],
 
-        "sobremesa-inconfidentes"=>[
-            "titulo"=>"SOBREMESA INCONFIDENTES ",
-            "ingredientes"=>"Sorvete de queijo artesanal, com geleia de goiaba e hortelã ",
-            "tempo"=>"Tempo médio de preparo 20 minutos ",
-            "sugestao"=>"prato harmoniza com soda limonada italiana",
+        "pastel-angu" => [
+            "titulo" => "PETISCO PASTEL DE ANGU TROPEIRO ",
+            "ingredientes" => " Sabores banana, carne, queijo, frango com passas , milho e queijo",
+            "tempo" => " Tempo médio de preparo 15 minutos",
+            "sugestao" => " Prato harmoniza com caldo de cana com limão ou abacaxi",
         ],
-        "torta-ouropreto"=>[
-            "titulo"=>"TORTA OURO PRETO",
-            "ingredientes"=>"Torta de chocolate amargo, com frutas vermelhas ",
-            "tempo"=>"Tempo médio de preparo 20 minutos",
-            "sugestao"=>"prato harmoniza soda italiana de maçã",
+        "pernil-serro" => [
+            "titulo" => "PRATO PERNIL AO MOLHO DE GOIABA DO SERRO ",
+            "ingredientes" => "Pernil marinado por 24 horas em especiarias da casa, risoto de queijo do serro , com molho de goiaba ",
+            "tempo" => " Tempo médio de preparo 45 minutos",
+            "sugestao" => "Prato harmoniza com vinho branco seco ",
         ],
-        ];
+
+        "sobremesa-inconfidentes" => [
+            "titulo" => "SOBREMESA INCONFIDENTES ",
+            "ingredientes" => "Sorvete de queijo artesanal, com geleia de goiaba e hortelã ",
+            "tempo" => "Tempo médio de preparo 20 minutos ",
+            "sugestao" => "prato harmoniza com soda limonada italiana",
+        ],
+        "torta-ouropreto" => [
+            "titulo" => "TORTA OURO PRETO",
+            "ingredientes" => "Torta de chocolate amargo, com frutas vermelhas ",
+            "tempo" => "Tempo médio de preparo 20 minutos",
+            "sugestao" => "prato harmoniza soda italiana de maçã",
+        ],
+    ];
 
     echo '<pre>';
-    print_r($pratos);
+    //print_r($pratos);
+    foreach ($pratos as $idPrato => $descricao) {
+        print_r($idPrato);
+        echo '<br>';
+        print_r($descricao["titulo"]);
+        echo '<br>';
+    }
     echo '</pre>';
 
     ?>
-    <section id="file-sabara">
-        <h2> <b> PRATO FILÉ AO MOLHO SABARÁ</b> </h2>
-        <p><i> Medalhão de Filé, flambado na cachaça, com puré de mandioca ao molho de jabuticaba</i> </p>
-        <p> Tempo médio de preparo 30 minutos</p>
-        <p> Prato harmoniza com vinho branco seco</p>
-    </section>
-    <section id="tilapia-tabuleiro ">
-        <h2> <b> PRATO TILÁPIA A MODA CACHOEIRA TABULEIRO</b> </h2>
-        <p><i> Filé de Tilápia no limão, acompanha salada de repolho com abacaxi e maçã e arroz branco ao alho</i> </p>
-        <p> Tempo médio de preparo 20 minutos</p>
-        <p> Prato harmoniza com vinho branco seco</p>
-    </section>
-    <section id=" costelinha-tiradentes">
-        <h2> <b> PRATO COSTELINHA A MODA TIRADENTES</b> </h2>
-        <p><i> Costelinha banhada na cachaça, acompanha couve frita, lascas de mandioca com molho de pimenta biquinho</i> </p>
-        <p> Tempo médio de preparo 40 minutos</p>
-        <p> Prato harmoniza com vinho suave</p>
+    <?php
 
-        <section id=" serra-caraca">
-            <h2> <b>PETISCO SERRA DO CARAÇA</b> </h2>
-            <p><i> Bolinho de arroz recheado com queijo e goiabada</i> </p>
-            <p> Tempo médio de preparo 20 minutos</p>
-            <p> Prato harmoniza com vinho de jabuticada</p>
+    foreach ($pratos as $idPrato => $descricao) {
+        echo '<section id="' . $idPrato . '">';
+        echo '<h2> <b>' . $descricao["titulo"] . '</b> </h2>';
+        echo '<p><i>' . $descricao["ingredientes"] . '</i> </p>';
+        echo '<p>' . $descricao["tempo"] . '</p>';
+        echo '<p>' . $descricao["sugestao"] . '</p>';
+        echo '</section>';
+    }
 
-            <section id=" frango-diamantina">
-                <h2> <b>PRATO SURPRESA DIAMANTINA DE FRANGO</b> </h2>
-                <p><i> Bombom de frango, envolto em bacon e queijo banhado ao molho de mostarda e mel</i> </p>
-                <p> Tempo médio de preparo 20 minutos</p>
-                <p> Prato harmoniza com vinho suave</p>
+    ?>
 
-                <section id=" pastel-angu">
-                    <h2> <b> PETISCO PASTEL DE ANGU TROPEIRO</b> </h2>
-                    <p><i> Sabores banana, carne, queijo, frango com passas , milho e queijo</i> </p>
-                    <p> Tempo médio de preparo 15 minutos</p>
-                    <p> Prato harmoniza com caldo de cana com limão ou abacaxi</p>
-
-                    <section id="  pernil-serro">
-                        <h2> <b> PRATO PERNIL AO MOLHO DE GOIABA DO SERRO </b> </h2>
-                        <p><i> Pernil marinado por 24 horas em especiarias da casa, risoto de queijo do serro , com molho de goiaba</i> </p>
-                        <p> Tempo médio de preparo 45 minutos</p>
-                        <p> Prato harmoniza com vinho branco seco</p>
-
-                        <section id="sobremesa-inconfidentes">
-                            <h2> <b>SOBREMESA INCONFIDENTES </b> </h2>
-                            <p><i> Sorvete de queijo artesanal, com geleia de goiaba e hortelã</i> </p>
-                            <p> Tempo médio de preparo 20 minutos</p>
-                            <p> prato harmoniza com soda limonada italiana</p>
-
-                            <section id=" torta-ouropreto">
-                                <h2> <b>TORTA OURO PRETO</b> </h2>
-                                <p><i> Torta de chocolate amargo, com frutas vermelhas</i> </p>
-                                <p> Tempo médio de preparo 20 minutos</p>
-                                <p> prato harmoniza soda italiana de maçã</p>
-
-                            </section>
-
-                        </section>
-
-
-
-
-                    </section>
-
-
-                </section>
-
-
-            </section>
-
-        </section>
-
-
-
-    </section>
-
-
-    </section>
 </body>
 
 </html>
